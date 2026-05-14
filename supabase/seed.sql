2b24bc923b2bec9a81ac40b0e2c6690f7c873de1
not-sol <castones.sol@gmail.com>
2026-05-15 00:15:59 +0800

    feat: refactor and some fixes

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,3 +1,12 @@
+-- Seed departments
+insert into public.departments (department_name, college_name)
+values 
+  ('CSMOD', 'College of Science and Mathematics'),
+  ('DBSES', 'College of Science and Mathematics'),
+  ('DFSC', 'College of Science and Mathematics'),
+  ('DMPCS', 'College of Science and Mathematics')
+on conflict (department_name) do nothing;
+
 delete from public.accomplishment_reports
 where remarks like 'Dummy seed:%';
 
